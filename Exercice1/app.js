cf804f731dfe60d639a9522ed8a17c1c3500d79b
mqthys62 <mathysthery@gmail.com>
2023-01-13 09:44:14 +0100

    mise a jour

--- Exercice1/app.js
+++ Exercice1/app.js
@@ -3,44 +3,45 @@ contactForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const firstNameInput = document.getElementById('firstName');
-    const lastNameInput = document.getElementById('lastName');
+    const passwordInput = document.getElementById('password');
     const emailInput = document.getElementById('emailInput');
     const phoneInput = document.getElementById('phoneInput');
 
     const formData = {
         firstName: firstNameInput.value,
-        lastName: lastNameInput.value,
+        password: passwordInput.value,
         email: emailInput.value,
         phone: phoneInput.value, 
     }
 
     const errors = {
         firstName: false,
-        lastName: false,
+        password: false,
         email: false,
         phone: false,
     }
 
     const firstNameError = document.getElementById('firstNameError');
-    const lastNameError = document.getElementById('lastNameError');
+    const passwordError = document.getElementById('passwordError');
     const phoneError = document.getElementById('phoneError');
     const emailError = document.getElementById('emailError');
     firstNameError.style.display = 'none';
-    lastNameError.style.display = 'none';
+    passwordError.style.display = 'none';
     emailError.style.display = 'none';
     phoneError.style.display = 'none';
 
-    if (!formData.firstName || !formData.lastName || !formData.email || !formData.phone || !formData.message) {
-        const emailRegex = /[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/igm;
+    if (!formData.firstName || !formData.password || !formData.email || !formData.phone ) {
         const nameRegex = /^[a-zA-Z ]+$/
+        const emailRegex = /[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/igm;
         const phoneRegex = /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/
+        const passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8})$/
         if (!formData.firstName || !nameRegex.test(formData.firstName)) {
             errors.firstName = true;
             firstNameError.style.display = 'block';
         }
-        if (!formData.lastName || !nameRegex.test(formData.lastName)) {
-            errors.lastName = true;
-            lastNameError.style.display = 'block';
+        if (!formData.password || !passwordRegex.test(formData.password)) {
+            errors.password = true;
+            passwordError.style.display = 'block';
         }
         if (!formData.email || !emailRegex.test(formData.email)) {
             errors.email = true;
